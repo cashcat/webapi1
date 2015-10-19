53fdcedeee8702a0d8c197cbd8045a649d86b547
cashcat <cashcat@gmail.com>
2015-10-19 23:38:43 +0800

    添加EF相关模型

---
+++ ConsoleAppTest/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Process();
+            Console.Read();
+        }
+
+        private async static void Process()
+        {
+            HttpClient httpClient = new HttpClient();
+            HttpResponseMessage response = await httpClient.GetAsync("http://localhost/apiwebsite/api/values/5");
+            string values = await response.Content.ReadAsStringAsync();
+            Console.WriteLine("返回值:"+values);
+        }
+    }
+}
---
+++ WebApi/Controllers/ValuesController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace WebApi.Controllers
+{
+    public class ValuesController : ApiController
+    {
+        // GET api/values
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "参数值1", "参数值2" };
+        }
+
+        // GET api/values/5
+        public string Get(int id)
+        {
+            return "value"+id.ToString();
+        }
+
+        // POST api/values
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT api/values/5
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE api/values/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
